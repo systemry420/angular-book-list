872526c8966aac1fb1cf06e5e9a67b15e64c870f
Hasan <systemry420@gmail.com>
2020-12-17 20:52:21 +0200

    generate unique id for each created book

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class BookService {
   books = [
-    { title: "Harry Potter", author: "J. K. Rowling", id: 1},
-    { title: "The Alchemist", author: "Paulo Cuelho", id: 2},
+    // { title: "Harry Potter", author: "J. K. Rowling", id: '1'},
+    // { title: "The Alchemist", author: "Paulo Cuelho", id: '2'},
   ]
   constructor() { }
 
@@ -15,13 +15,22 @@ export class BookService {
   }
 
   addBook(title: string, author: string) {
-    this.books.push({title, author, id:3})
-
+    this.books.push({ title, author, id: this.getUniqueId(2) })
   }
 
   deleteBook(id) {
     this.books = this.books.filter(b => {
       return b.id != id
     })
+  }
+
+  getUniqueId(parts: number): string {
+    const stringArr = [];
+    for(let i = 0; i< parts; i++){
+      // tslint:disable-next-line:no-bitwise
+      const S4 = (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+      stringArr.push(S4);
+    }
+    return stringArr.join('-');
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _bookService: BookService) { }
 
   ngOnInit(): void {
+  }
+
+  getCount() {
+    return this._bookService.getBooks().length
   }
 
 }
