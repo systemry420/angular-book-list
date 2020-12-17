9ae0a035e471599f49f64f18c5f6662bd0ea43c2
Hasan <systemry420@gmail.com>
2020-12-17 18:12:38 +0200

    initial markup & style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookformComponent } from './bookform/bookform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BooklistComponent,
+    BookformComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/bookform/bookform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookform',
+  templateUrl: './bookform.component.html',
+  styleUrls: ['./bookform.component.css']
+})
+export class BookformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
